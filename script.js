1f16dee3be9f01782dd73f070ab6e00b1dac480e
inesstkhay <106350943+inesstkhay@users.noreply.github.com>
2024-03-14 10:24:23 +0100

    accents et police

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textElement = document.getElementById('loading-text');
     const textToType = "<div style='display: inline-block;'><img src='bios1.png' alt='bios'></div>" +
-                      "<div style='display: inline-block; vertical-align: top;'>\tINESS TKHAYYARE V1.0 2024\n \tPORTFOLIO NUMÉRIQUE\n</div>" +
+                      "<div style='display: inline-block; vertical-align: top;'>\tINESS TKHAYYARE V1.0 2024\n \tPORTFOLIO NUMERIQUE\n</div>" +
                       "\nInstitution \t\t\t\t\t\t :ENSA Toulouse\n" +
                       "Spécialité \t\t\t\t\t\t :Architecture + Numérique\n" +
                       "Temps passé à concevoir \t\t :100000\n" +
